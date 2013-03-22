e038f3f571e59536161a2cabde85c9c79e9d9ed3
morganwilde <wilde.morgan@gmail.com>
2013-03-22 13:18:30 +0200

    added dbcreds

--- home.py
+++ home.py
@@ -3,11 +3,12 @@ from flask import Flask
 
 app = Flask(__name__)
 
+@app.route('/<path:path>')
 @app.route('/')
-def hello():
-    return 'Hello World!'
+def begin(path = '/'):
+    tempReturn = ''
+    return tempReturn
 
 if __name__ == '__main__':
-    # Bind to PORT if defined, otherwise default to 5000.
     port = int(os.environ.get('PORT', 5000))
     app.run(host = '0.0.0.0', port = port, debug = True)
